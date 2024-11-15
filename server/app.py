0d268bda8f49f9a315a9cad48aa2c6e27b12dc9b
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-11-15 11:13:43 -0600

    Complete lab

--- server/app.py
+++ server/app.py
@@ -23,12 +23,18 @@ def bakeries():
     bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
     return make_response(  bakeries,   200  )
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods=["GET", "PATCH"])
 def bakery_by_id(id):
-
-    bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
-    return make_response ( bakery_serialized, 200  )
+    if request.method == "GET":
+        bakery = Bakery.query.filter_by(id=id).first()
+        bakery_serialized = bakery.to_dict()
+        return make_response ( bakery_serialized, 200 )
+    elif request.method == "PATCH": 
+        bakery = Bakery.query.filter_by(id=id).first()
+        for attr in request.form:
+            setattr(bakery, attr, request.form.get(attr))
+        db.session.commit()
+        return make_response(bakery.to_dict(), 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
@@ -44,6 +50,32 @@ def most_expensive_baked_good():
     most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
     most_expensive_serialized = most_expensive.to_dict()
     return make_response( most_expensive_serialized,   200  )
+
+@app.route("/baked_goods", methods=["POST"])
+def baked_goods_post():
+    if request.method == "POST":
+        new_baked_good = BakedGood(
+            name=request.form.get("name"),
+            price=request.form.get("price"),
+            bakery_id=request.form.get("bakery_id")
+        )
+        db.session.add(new_baked_good)
+        db.session.commit()
+
+        return make_response(
+            new_baked_good.to_dict(),
+            201
+        )
+
+@app.route("/baked_goods/<int:id>", methods=["DELETE"])
+def baked_goods_delete(id):
+    baked_good = BakedGood.query.filter_by(id=id).first()
+
+    if baked_good:
+        db.session.delete(baked_good)
+        return make_response({ "message": "record successfully deleted" }, 200)
+    else:
+        return make_response({ "message": f"baked good {id} not found" }, 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
